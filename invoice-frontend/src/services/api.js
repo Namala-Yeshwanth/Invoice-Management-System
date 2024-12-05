2f57dbc50d505d6c178904c945528105baad0c30
Namala-Yeshwanth <namalayeshwanth41@gmail.com>
2024-12-05 11:04:44 +0530

    changed Api url in api.js

--- invoice-frontend/src/services/api.js
+++ invoice-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';  // Adjust this URL based on your Django server
+const API_URL = 'https://invoice-management-system-qbm4.onrender.com/api';  // Adjust this URL based on your Django server
 
 // // Get all invoices
 // export const getInvoices = async (page=1, rowsPerPage=5) => {
