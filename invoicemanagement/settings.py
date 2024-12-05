db88c27df81f060a3a94a81d4fbec03d2f70b13a
Namala-Yeshwanth <namalayeshwanth41@gmail.com>
2024-12-05 10:32:40 +0530

    updated databases adding defualt

--- invoicemanagement/settings.py
+++ invoicemanagement/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'invoicemanagement.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600, ssl_require=True)
 }
 
 
